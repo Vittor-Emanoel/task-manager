dc3a8870a0bc5c8850697b8729b3b5a129aadeea
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2025-09-22 07:49:45 -0300

    validacao no modal de criacao

--- apps/web/src/components/Modals/CreateTaskModal/index.tsx
+++ apps/web/src/components/Modals/CreateTaskModal/index.tsx
@@ -1,6 +1,13 @@
-import { useForm } from "@tanstack/react-form";
-import { CheckCheckIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Field, useForm } from "@tanstack/react-form";
+import { CheckCheckIcon, ChevronDownIcon } from "lucide-react";
 import { useState } from "react";
+import { undefined } from "zod";
 import { Button } from "../../ui/button";
 import {
   Dialog,
@@ -21,7 +28,6 @@ import {
 } from "../../ui/select";
 import { Textarea } from "../../ui/textarea";
 import { RecurringCard } from "./components/RecurringCard";
-import { ScheduleCard } from "./components/ScheduleCard";
 type TaskType = "single" | "recurring";
 
 interface ICreateTaskModalProps {
@@ -30,11 +36,18 @@ interface ICreateTaskModalProps {
 
 export const CreateTaskModal = ({ children }: ICreateTaskModalProps) => {
   const [taskType, setTaskType] = useState<TaskType>("single");
+  const [categoryId, setCategoryId] = useState("1");
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Date | undefined>(undefined);
 
   const form = useForm({
     defaultValues: {
       name: "",
       description: "",
+      task_type: "single",
+      categoryId: "single",
+      scheduledDate: new Date() ?? undefined,
+      scheduledTime: undefined,
     },
     onSubmit: ({ value }) => {
       console.log(value, "value");
@@ -100,43 +113,119 @@ export const CreateTaskModal = ({ children }: ICreateTaskModalProps) => {
 
             <div className="grid grid-cols-2 gap-4">
               <div className="space-y-2">
-                <Label>Tipo de Tarefa</Label>
-                <Select
-                  value={taskType}
-                  onValueChange={(value: "single" | "recurring") =>
-                    setTaskType(value)
-                  }
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="single">Tarefa única</SelectItem>
-                    <SelectItem value="recurring">Tarefa recorrente</SelectItem>
-                  </SelectContent>
-                </Select>
+                <Field name="task_type" form={form}>
+                  {(field) => (
+                    <>
+                      <Label htmlFor={field.name}>Tipo de Tarefa</Label>
+                      <Select
+                        name={field.name}
+                        value={field.state.value}
+                        onValueChange={(value: TaskType) => {
+                          field.handleChange(value);
+                          setTaskType(value);
+                        }}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="single">Tarefa única</SelectItem>
+                          <SelectItem value="recurring">
+                            Tarefa recorrente
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </>
+                  )}
+                </Field>
               </div>
               <div className="space-y-2">
-                <Label>Categoria</Label>
-                <Select
-                  value={taskType}
-                  onValueChange={(value: "single" | "recurring") =>
-                    setTaskType(value)
-                  }
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="single">Tarefa única</SelectItem>
-                    <SelectItem value="recurring">Tarefa recorrente</SelectItem>
-                  </SelectContent>
-                </Select>
+                <Field name="categoryId" form={form}>
+                  {(field) => (
+                    <>
+                      <Label htmlFor={field.name}>Categoria</Label>
+                      <Select
+                        value={categoryId}
+                        onValueChange={(value) => {
+                          field.handleChange(value);
+                          setCategoryId(value);
+                        }}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="1">Trabalho</SelectItem>
+                          <SelectItem value="2">Pessoal</SelectItem>
+                          <SelectItem value="3">home</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </>
+                  )}
+                </Field>
               </div>
             </div>
 
             <div className="border-t border rounded-md">
-              {taskType === "single" && <ScheduleCard />}
+              {taskType === "single" && (
+                <div className="space-y-3 p-3 bg-zinc-100 rounded-lg">
+                  <Label className="text-sm font-medium">Agendamento</Label>
+                  <div className="flex gap-4">
+                    <div className="flex flex-col gap-3 w-full">
+                      <form.Field name="scheduledDate" form={form}>
+                        {(field) => (
+                          <>
+                            <Label htmlFor={field.name} className="px-1">
+                              Data
+                            </Label>
+                            <Popover open={open} onOpenChange={setOpen}>
+                              <PopoverTrigger asChild>
+                                <Button
+                                  variant="outline"
+                                  id="date-picker"
+                                  className="w-full justify-between font-normal"
+                                >
+                                  {date
+                                    ? date.toLocaleDateString()
+                                    : "Select date"}
+                                  <ChevronDownIcon />
+                                </Button>
+                              </PopoverTrigger>
+                              <PopoverContent
+                                className="overflow-hidden p-0 w-full"
+                                align="start"
+                              >
+                                <Calendar
+                                  mode="single"
+                                  selected={field.state.value}
+                                  captionLayout="dropdown"
+                                  onSelect={(date) => {
+                                    field.handleChange(date);
+                                    setDate(date);
+                                    setOpen(false);
+                                  }}
+                                />
+                              </PopoverContent>
+                            </Popover>
+                          </>
+                        )}
+                      </form.Field>
+                    </div>
+                    <div className="flex flex-col gap-3 w-full">
+                      <Label htmlFor="time-picker" className="px-1">
+                        Horario
+                      </Label>
+                      <Input
+                        type="time"
+                        id="time-picker"
+                        step="1"
+                        defaultValue="10:30:00"
+                        className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none w-full"
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
               {taskType === "recurring" && <RecurringCard />}
             </div>
 
